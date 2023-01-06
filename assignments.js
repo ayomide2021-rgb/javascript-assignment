6db5b4e438e667b12d5c02001ac93d3e2d4f7dc9
Ogundana Ayomide <89689966+ayomide2021-rgb@users.noreply.github.com>
2023-01-06 08:16:34 +0100

    Add files via upload

---
+++ assignments.js
@@ -0,0 +1,68 @@
+// 10. Values and Variables
+
+let country = "Nigeria";
+console.log (country);
+
+let continent = "Africa";
+console.log(continent);
+
+let population = 180000000;
+console.log(population);
+
+// 12. Data Types
+let isIsland = "Nigeria";
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof language);
+
+// 13. let, const and var
+let countryLanguage = "Yoruba";
+console.log (countryLanguage);
+
+const assTitle = "JavaScript";
+console.log (assTitle);
+// let asstitle = "Udemy"
+// console.log (assTitle);
+
+// 14. basic operators
+let myCountryPopulation = 180000000 / 2;
+myCountryPopulation ++;
+console.log(myCountryPopulation);
+
+let finLandPopulation = 6000000;
+console.log (myCountryPopulation > finLandPopulation);
+
+let averageCountryPopulation = 33000000;
+console.log (myCountryPopulation < averageCountryPopulation);
+
+let description = " Portugal is in Europe, and its 11 million people speak portugese"
+
+// 14. code challenge
+
+//DATA ONE
+/*const massMark = 78;
+const massJohn = 92;
+const heightMark = 1.69;
+const heightJohn = 1.95;
+
+const dataOneBMIMark = (massMark / heightMark ** 2 );
+const dataOneBMIJohn = massJohn / (heightJohn * heightJohn ) ;
+const markHigherBMI = (dataOneBMIMark > dataOneBMIJohn)
+console.log (dataOneBMIMark ,dataOneBMIJohn);
+console.log (markHigherBMI);
+*/
+
+//DATA TWO
+const massMark = 95;
+const massJohn = 85;
+const heightMark = 1.88;
+const heightJohn = 1.76;
+
+const dataOneBMIMark = (massMark / heightMark ** 2 );
+const dataOneBMIJohn = massJohn / (heightJohn * heightJohn ) ;
+const markHigherBMI = (dataOneBMIMark > dataOneBMIJohn)
+console.log (dataOneBMIMark ,dataOneBMIJohn);
+console.log (markHigherBMI);
+
+// 14. operator Precedence
